ea9b0fb43c866efe9859f1fc3168325354628af4
investigacion 3 <el-aleman23@hotmail.com>
2016-08-30 08:41:18 -0500

    compilador c

---
+++ clase 2016 08 30.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int c;
+int d;
+printf ("inserte primer numero: \n");
+scanf("%d",&c);
+printf("inserte segundo numero: \n");
+scanf("%d",&d);
+c=c*d;
+printf(" el resultado de la operacion es: %d \n",c);
+}
+
---
+++ compilador.sh
@@ -0,0 +1,3 @@
+gcc clase\ 2016\ 08\ 30.c -o clase.exe
+./clase.exe
+
