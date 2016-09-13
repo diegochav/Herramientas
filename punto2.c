0e10aebd5ba5b26a481cf2b97100b1152ef9b5bf
investigacion 2 <el-aleman23@hotmail.com>
2016-09-13 08:55:28 -0500

    taller punto 2

--- punto2.c
+++ punto2.c
@@ -2,7 +2,7 @@
 int main(){
 
 int a=0,b=0,pos=0,neg=0,par=0,imp=0;
-for (a=0;a<10,a++){
+for (a=0;a<10;a++){
 	printf("Ingrese el valor: \n");
 	scanf("%d",&b);
 		if (b>=0){
@@ -13,16 +13,20 @@ for (a=0;a<10,a++){
 			else {
 				imp=imp+1;
 			}
+		}
 		else {
 		neg=neg+1;
+			if(b%2==0){
+				par=par+1;
+			}
+			else{
+				imp=imp+1;
+			}
 		}	
 }
 printf("\nLos numeros positivos son: %d\n",pos);
-
-
-
-
-
-
+printf("\nLos numeros negativos son: %d\n",neg);
+printf("\nLos numeros pares son: %d\n",par);
+printf("\nLos numeros impares son: %d\n",imp);
 
 }
