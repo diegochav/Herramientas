78ed1511a511910d1d675f5bb997663d2866086f
investigacion 2 <el-aleman23@hotmail.com>
2016-09-13 08:37:27 -0500

    taller punto 2

---
+++ punto2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+
+int a=0,b=0,pos=0,neg=0,par=0,imp=0;
+for (a=0;a<10,a++){
+	printf("Ingrese el valor: \n");
+	scanf("%d",&b);
+		if (b>=0){
+		pos=pos+1;
+			if(a%2==0){
+				par
+
+
+
+
+
+
+
+}
