02d8efb2d107d8bb19ec9502efb7968fd87076f0
investigacion 2 <el-aleman23@hotmail.com>
2016-09-15 08:53:18 -0500

    taller 2 punto 2

--- t2punto2.c
+++ t2punto2.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main(){
 int a,b,c,d;
+while (a!=6){
 printf("Menu \n 1.suma \n 2.Resta \n 3.Multiplicacion \n 4.Div \n 5.Potencia \n 6.Salir \n");
 scanf("%d",&a);
 printf("Ingrese primer numero: \n");
@@ -11,25 +12,19 @@ switch (a){
 	case 1:
 		d=b+c;
 		printf("La suma es: %d\n",d);
+	break;
 	case 2:
 		d=b-c;
 		printf("La resta es: %d\n",d);
+	break;
 	case 3:
 		d=b*c;
 		printf("La Multiplicacion es: %d\n",d);
-	case 4:
+	break;
 		
 }
-
-
-
-
-
-
-
-
 }
-
+}
 
 
 
