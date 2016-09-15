8336b7f2652570a505709e68c00e454f9770a8df
investigacion 2 <el-aleman23@hotmail.com>
2016-09-15 08:36:29 -0500

    taller 2 punto 2

--- t2punto2.c
+++ t2punto2.c
@@ -1,5 +1,35 @@
 #include<stdio.h>
 int main(){
+int a,b,c,d;
 printf("Menu \n 1.suma \n 2.Resta \n 3.Multiplicacion \n 4.Div \n 5.Potencia \n 6.Salir \n");
+scanf("%d",&a);
+printf("Ingrese primer numero: \n");
+scanf("%d",&b);
+printf("Ingrese segundo numero: \n");
+scanf("%d",&c);
+switch (a){
+	case 1:
+		d=b+c;
+		printf("La suma es: %d\n",d);
+	case 2:
+		d=b-c;
+		printf("La resta es: %d\n",d);
+	case 3:
+		d=b*c;
+		printf("La Multiplicacion es: %d\n",d);
+	case 4:
+		
 }
+
+
+
+
+
+
+
+
+}
+
+
+
 
