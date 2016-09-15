093a9bbc39c779481a4157a3dcadb51a72411ca7
investigacion 2 <el-aleman23@hotmail.com>
2016-09-15 07:45:10 -0500

    taller punto 3

--- punto3.c
+++ punto3.c
@@ -6,13 +6,13 @@ scanf("%c%*c",&letra1);
 printf("Dame otra letra: \n");
 scanf("%c",&letra2);
 printf("Las letras son: \n");
+while((letra2<letra1)){
+printf("%3c",letra2);
+letra2++;
+}
 while((letra1<=letra2)){
 printf("%3c",letra1);
 letra1++;
-}
-while((letra2<=letra1)){
-printf("%3c",letra2);
-letra2++;
 }
 printf("\n");
 return 0;
