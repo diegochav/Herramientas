f1fd661257f263ee700b15d5368391df0ea027f8
diegochav <el-aleman23@hotmail.com>
2016-09-28 20:12:07 -0500

    programa terminado

--- t2punto2.c
+++ t2punto2.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-int a,b,c,d;
+int a,b,c,d,e,g,f;
 while (a!=6){
 printf("Menu \n 1.suma \n 2.Resta \n 3.Multiplicacion \n 4.Div \n 5.Potencia \n 6.Salir \n");
 scanf("%d",&a);
@@ -20,11 +20,24 @@ switch (a){
 	case 3:
 		d=b*c;
 		printf("La Multiplicacion es: %d\n",d);
-	break;
-		
+		break;
+	case 4:
+		d=b/c;
+		printf("La division es: %d\n",d);
+		break;
+	case 5:
+		d=1;
+		g=b;
+		e=1;
+		for(f=d;f<=c;f++){
+			e=g*e;
+		}
+		printf("La potencia es: %d\n",e);
+		break;
+	case 6:
+		printf("Ha salido del programa\n");
 }
 }
 }
-
 
 
