600a7326061a9f940fd6eb066a578dfb250d640d
diegochav <el-aleman23@hotmail.com>
2016-09-28 22:14:17 -0500

    potencia

--- calculadora.c
+++ calculadora.c
@@ -3,30 +3,60 @@
 int suma(int a, int b);
 int resta(int a, int b);
 int multiplicacion(int a, int b);
+int potencia(int a, int b);
+//int factorial(int a);
 
 int main(){
 	int numero1, numero2, operacion,resultado;
 	while(1){
-  	printf("Escriba primer numero\n");
-	scanf("%d",&numero1);
-	printf("Escriba segundo numero\n");
-	scanf("%d",&numero2);
-	printf("Escriba la operacion a realizar:\n 1:Suma\n 2:Resta\n 3:Multiplicacion\n 4:Salir \n");
+	printf("CALCULADORA\n 1:Suma\n 2:Resta\n 3:Multiplicacion\n 4:Potencia\n 5:Factorial\n 6:Salir\n Escriba el numero de la operacion a realizar: ");
 	scanf("%d",&operacion);
 	switch (operacion){
 		case 1:
+			printf("Escriba primer numero:\n");
+			scanf("%d",&numero1);
+			printf("Escriba segundo numero:\n");
+			scanf("%d",&numero2);
                 	resultado=suma(numero1, numero2);
 			printf("El resultado es: %d\n ",resultado);
 			break;
 		case 2:
+			printf("Escriba primer numero:\n");
+			scanf("%d",&numero1);
+			printf("Escriba segundo numero:\n");
+			scanf("%d",&numero2);
 			resultado=resta(numero1, numero2);
 			printf("El resultado es: %d\n",resultado);
 			break;
 		case 3:
+			printf("Escriba primer numero:\n");
+			scanf("%d",&numero1);
+			printf("Escriba segundo numero:\n");
+			scanf("%d",&numero2);
 			resultado=multiplicacion(numero1, numero2);
 			printf("El resultado es: %d\n",resultado);
 			break;
 		case 4:
+			printf("Escriba un numero:\n");
+			scanf("%d",&numero1);
+			printf("Escriba la potencia para el numero anterior:\n");
+			scanf("%d",&numero2);
+			resultado=potencia(numero1, numero2);
+			printf("El resultado es: %d\n", resultado);
+			break;
+		//case 5:
+		//	printf("Escriba un numero:\n");
+		//	scanf("%d",&numero1);
+		//	resultado=factorial(numero1);
+		//	printf("El resultado es: %d\n", resultado);
+		//	break;
+
+
+
+
+
+
+		case 6:
 			printf("Ha salido de la calculadora\n");
 			return 0;
 			break;
@@ -48,7 +78,16 @@ int multiplicacion(int numero1, int numero2){
 		}
 	return a;
 }
-
-
-
+int potencia(int numero1, int numero2){
+	int a=numero1;
+	int b=0;
+	for(int i=0;i<numero2;i++){
+		a=a+b;
+		b=0;
+		for(int f=1;f<numero1;f++){
+			b=b+a;
+		}
+	}
+	return a;
+}
 
