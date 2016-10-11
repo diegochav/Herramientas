9c213739d41d72c9e772d5d4f498006274f0e6cc
investigacion 2 <el-aleman23@hotmail.com>
2016-10-11 08:48:20 -0500

    punto 3 parcial en c

---
+++ parcialp3.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int  main(){
+int a,c,d,f,g;
+printf("Escriba el numero de bases: ");
+	scanf("%d",&a);
+f=0;
+for(int i=0;i<a;i++){
+	printf("Escriba una base: ");
+	scanf("%d",&d);
+	f=d+f;
+}
+
+printf("Ingrese Modulo: ");
+scanf("%d\n",&c)
+
+pritnf("1:suma\n 2:resta\n 3:multiplicaion\n");
+printf("%d",&g);
+
+switch (g){
+	case 1:
+		d=(f%2)
+	case 2:
+		d=f-19;
+	case 3:
+		d=f-13;
+printf("Sumatoria: %d",f);
+
+printf("promedio: %d\n",d);
+}
+
+
