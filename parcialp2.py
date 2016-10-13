991ac37af737c42bb4630114740fe26c0cc860d5
diegochav <el-aleman23@hotmail.com>
2016-10-12 22:54:39 -0500

    punto 2 python

---
+++ parcialp2.py
@@ -0,0 +1,13 @@
+a = int(input("Escriba el radio: "))
+f=2
+h=3.14
+b = float(a*f)
+c = float(b*h)
+d = float((a*a)*h)
+
+print("El diametro es: "+str(b))
+
+print("La circunferencia es : "+str(c))
+
+print("El area es: "+str(d))
+
