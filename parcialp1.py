4f50e94c0a94a4e49f4623b2be8e4b49e80630a6
diegochav <el-aleman23@hotmail.com>
2016-10-12 22:54:09 -0500

    punto 1 python

---
+++ parcialp1.py
@@ -0,0 +1,11 @@
+a = int(input("Escriba un numero de datos: "))
+f=0
+for i in range(a):
+	d = int(input("Escriba dato: "))
+	f =int( f + d )
+print("La sumatoria es: "+str(f))
+c = int(f/a)
+print("El promedio es: "+str(c))
+
+
+
