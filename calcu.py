04e297cbdd120dad6d3ee045a303d1d2789d28c7
investigacion 2 <el-aleman23@hotmail.com>
2016-11-01 07:50:32 -0500

    funcionando

--- calcu.py
+++ calcu.py
@@ -59,10 +59,7 @@ pass
 
 def on_butondivclick():
 	texto = name.get()
-#	name.set(texto+"/")
-	if (texto == "/"):
-		resultado= texto / texto
-		name.set(str(resultado))
+	name.set(texto+"/")
 pass
 
 def on_butonmultclick():
@@ -82,20 +79,27 @@ pass
 
 def on_butonceclick():
 	texto = name.get()
-	name.set(texto+"CE")
+	name.set(" ")
 pass
 
 def on_butonigualclick():
 	texto = name.get()
-
-	if  texto == "=":
-		calculo=eval(name.get())
-		name.set("="+str(calculo))
-	#name.set(texto)
-
+	calculo =0
+	calculo=eval(name.get())
+	name.set("="+str(calculo))
 
 
 win = tk.Tk()
+win.title("CALCULADORA")
+win.resizable(0,0)
+
+label1=ttk.Label(win,text="CALCULADORA")
+label1.grid(row=0,column=5)
+label2=ttk.Label(win,text=" ")
+label2.grid(row=3,column=5)
+label3=ttk.Label(win,text=" ")
+label3.grid(row=1,column=5)
+
 
 boton7=ttk.Button(win,text="7",comman=on_buton7click)
 boton7.grid(row=4,column=3)
@@ -118,16 +122,16 @@ boton2.grid(row=6,column=4)
 boton0=ttk.Button(win,text="0",comman=on_buton0click)
 boton0.grid(row=7,column=4)
 
-boton9=ttk.Button(win,text="9",comman=on_buton9click)
+boton9=ttk.Button(win,text="9",comman=on_buton9click,width=19)
 boton9.grid(row=4,column=5)
 
-boton6=ttk.Button(win,text="6",comman=on_buton6click)
+boton6=ttk.Button(win,text="6",comman=on_buton6click,width=19)
 boton6.grid(row=5,column=5)
 
-boton3=ttk.Button(win,text="3",comman=on_buton3click)
+boton3=ttk.Button(win,text="3",comman=on_buton3click,width=19)
 boton3.grid(row=6,column=5)
 
-botonpunto=ttk.Button(win,text=".",comman=on_butonpuntoclick)
+botonpunto=ttk.Button(win,text=".",comman=on_butonpuntoclick,width=19)
 botonpunto.grid(row=7,column=5)
 
 botondiv=ttk.Button(win,text="%",comman=on_butondivclick)
@@ -151,7 +155,7 @@ boton10.grid(row=5,column=7)
 
 name=tk.StringVar()
 txtentry=ttk.Entry(win,textvariable=name)
-txtentry.grid(row=1,column=5)
+txtentry.grid(row=2,column=5)
 
 
 win.mainloop()
